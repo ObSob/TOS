26657aeb099d083009848d6081690500ddbfac61
wbh <1762629204@qq.com>
2021-04-30 18:01:56 +0800

    display info when kernel panic

--- include/debug.h
+++ include/debug.h
@@ -7,7 +7,21 @@
 
 #include "console.h"
 #include "vargs.h"
+#include "elf.h"
 
+#define assert(x, info)                                       	\
+	do {                                                	\
+		if (!(x)) {                                     \
+			panic(info); 		     		\
+		}                                               \
+	} while (0)
+
+#define static_assert(x)                                	\
+	switch (x) { case 0: case (x): ; }
+
+void init_debug();
+void panic(const char *msg);
+void print_cur_status();
 void printk(const char *format, ...);
 void printk_color(real_color_t bg, real_color_t fore, const char* format, ...);
 
--- init/entry.c
+++ init/entry.c
@@ -4,7 +4,8 @@
 int kern_entry()
 {
 	console_clear();
-    printk_color(rc_black, rc_light_blue, "Hello, OS Kernel!\n", rc_black, rc_green);
+    printk_color(rc_black, rc_light_blue, "Hello, OS Kernel!\n");
+    panic("panic test");
 
 	return 0;
 }
